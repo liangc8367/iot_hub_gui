71ccc2886ce8e1e3f0ec79772e3f96f8912728ee
Your Name <you@example.com>
2018-05-12 22:42:52 -0400

    test matplot

--- iot_hub_gui.py
+++ iot_hub_gui.py
@@ -5,14 +5,70 @@ revision
 
 import os
 import sys
-import sqlite3
+import sqlite3 
+import random
 
+from PyQt5.QtWidgets import QApplication, QMainWindow, QMenu, QVBoxLayout, QSizePolicy, QMessageBox, QWidget, QPushButton
+from PyQt5.QtGui import QIcon
+ 
+from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
+from matplotlib.figure import Figure
 import matplotlib.pyplot as plt
+
 
 
 sqlite_file = "iot_sensor_data.db"
 
-def main():
+class IoTHubApp(QMainWindow):
+ 
+    def __init__(self):
+        super().__init__()
+        self.left = 10
+        self.top = 10
+        self.title = 'IoT Hub'
+        self.width = 640
+        self.height = 400
+        self.initUI()
+ 
+    def initUI(self):
+        self.setWindowTitle(self.title)
+        self.setGeometry(self.left, self.top, self.width, self.height)
+ 
+        m = PlotCanvas(self, width=5, height=4)
+        m.move(0,0)
+ 
+        button = QPushButton('PyQt5 button', self)
+        button.setToolTip('This s an example button')
+        button.move(500,0)
+        button.resize(140,100)
+ 
+        self.show()
+ 
+ 
+class PlotCanvas(FigureCanvas):
+ 
+    def __init__(self, parent=None, width=5, height=4, dpi=100):
+        fig = Figure(figsize=(width, height), dpi=dpi)
+        self.axes = fig.add_subplot(111)
+ 
+        FigureCanvas.__init__(self, fig)
+        self.setParent(parent)
+ 
+        FigureCanvas.setSizePolicy(self,
+                QSizePolicy.Expanding,
+                QSizePolicy.Expanding)
+        FigureCanvas.updateGeometry(self)
+        self.plot()
+ 
+ 
+    def plot(self):
+        data = [random.random() for i in range(25)]
+        ax = self.figure.add_subplot(111)
+        ax.plot(data, 'r-')
+        ax.set_title('PyQt Matplotlib Example')
+        self.draw()
+        
+def main2():
     sys.stdout.write("Hello\n")
     # load database
     conn = sqlite3.connect(sqlite_file)
@@ -35,4 +91,6 @@ def main():
 
 
 if __name__ == '__main__':
-    main()
+    app = QApplication(sys.argv)
+    ex = IoTHubApp()
+    sys.exit(app.exec_())
